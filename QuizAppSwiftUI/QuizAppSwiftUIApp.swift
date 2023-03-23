4a63499428713d171ac64280649bd71601e587a2
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2023-03-23 15:21:50 +0600

    Initial Commit

---
+++ QuizAppSwiftUI/QuizAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppSwiftUIApp.swift
+//  QuizAppSwiftUI
+//
+//  Created by Uchchhwas Roy on 23/3/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
